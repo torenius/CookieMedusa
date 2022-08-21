f039b8c3a5021413ce99b6d3987f047dfab5d232
Tommy Torenius <tommy.torenius@bokadirekt.se>
2022-08-21 17:05:21 +0200

    Hello World

---
+++ MySneak.cs
@@ -0,0 +1,20 @@
+﻿using Nadeko.Snake;
+using NadekoBot;
+using Discord;
+
+namespace CookieMedusa;
+
+public sealed class MySnek : Snek
+{
+    [cmd]
+    public async Task Hello(AnyContext ctx)
+    {
+        await ctx.Channel.SendMessageAsync($"Hello everyone!");
+    }
+
+    [cmd]
+    public async Task Hello(AnyContext ctx, IUser target)
+    {
+        await ctx.ConfirmLocalizedAsync("hello", target);
+    }
+}
